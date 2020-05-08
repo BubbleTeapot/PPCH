b3d2596a9ea8d193a29c2f70c38c7f248ad965e9
BubbleTeapot <2403379772@qq.com>
2020-05-08 18:33:56 +0800

    Header

--- CAO/src/components/Header/Header.vue
+++ CAO/src/components/Header/Header.vue
@@ -1,14 +1,27 @@
 <template>
   <div class="nav-wrp">
-    <div class="mn-main"></div>
-    <div class="mn-cld1 circle"></div>
-    <div class="mn-cld2"></div>
-    <div class="mn-cld3"></div>
+    <div class="mn-main bgCl1 shdCl1" @click="switchMenu()"></div>
+    <div class="circle bgCl1 shdCl1" :class="{'mn-cld1': opMenu}" @click="clMenu()"></div>
+    <div class="circle bgCl1 shdCl1" :class="{'mn-cld2': opMenu}" @click="clMenu()"></div>
+    <div class="circle bgCl1 shdCl1" :class="{'mn-cld3': opMenu}" @click="clMenu()"></div>
   </div>
 </template>
 <script>
 export default {
-  name: 'HeaderNav'
+  name: 'HeaderNav',
+  data() {
+    return {
+      opMenu: false,
+    }
+  },
+  methods: {
+    switchMenu() {
+      this.opMenu = !this.opMenu;
+    },
+    clMenu() {
+      this.opMenu = false;
+    }
+  }
 }
 </script>
 <style lang="less" src="./header.less" scoped></style>
